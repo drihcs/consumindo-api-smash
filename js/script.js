0563c8671d3e97dd7db84952c11bc410df46cbdf
Adriana Cruz <drih1502@gmail.com>
2025-05-05 16:21:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function showMessage(msg, type = "success") {
     messageArea.style.color = type === "error" ? "#e63946" : "#2a9d8f";
 }
 
-function upload() {
+function upload () {
     const input = document.getElementById("uploadInput");
     const files = [...input.files];
 
@@ -18,37 +18,36 @@ function upload() {
 
     const oversized = files.find(file => file.size > MAX_SIZE_MB * 1024 * 1024);
     if (oversized) {
-        showMessage(`Arquivo ${oversized.name} excede 5MB.`, "error");
+        showMessage (`Arquivo ${oversized.name} excede 5MB.`, "error");
         return;
     }
 
     showMessage("Enviando...", "success")
 
-    const su = new SmashUploader({
-        region: "us-east-1",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExYjdkODdjLTBkMTQtNGY0NS1hZDdlLWEzNDVmMTUyODEzYi1ldSIsInVzZXJuYW1lIjoiMTkwODgyN2MtNzU1NS00ZWNkLTlkNWItZjVjOTM0ZTRkNjNiIiwicmVnaW9uIjoidXMtZWFzdC0xIiwiaXAiOiIyMDEuNTkuMTA0Ljg5Iiwic2NvcGUiOiJOb25lIiwiYWNjb3VudCI6ImM4Zjk0ZjNiLTI4NWYtNGQ2Yy1iYTA5LTdlYTkwMTQzNDgxYS1lYSIsImlhdCI6MTc0NjQ3MTUzNCwiZXhwIjo0OTAyMjMxNTM0fQ.n8oLZ7LRsPrLANnfdumLBfXgnC40PJW6BIo6TzTweCA",
+    const su = new SmashUploader ({
+        region: "SUA_REGIAO",
+        token: "SUA_KEY",
     });
 
     su.upload({ files })
-        .then(result => {
-            const link = result?.transfer?.transferUrl;
+    .then(result => {
+        const link = result?.transfer?.transferUrl;
 
-            if (link) {
-                showMessage("Upload concluído!", "success");
-
-                files.forEach(file => {
-                    const li = document.createElement("li");
-                    li.innerHTML = `
+        if (link) {
+            showMessage("Upload concluído!", "success");
+    
+           files.forEach(file=> {
+            const li = document.createElement("li");
+            li.innerHTML = `
             ${file.name} —
-            <a href= "${link}" target="_blank"; ">Baixar</a>`;
-                    fileList.appendChild(li);
-                });
-            } else {
-                showMessage("Upload concluído, mas sem link.", "error");
-            }
-        })
+            <a href= "${link}" target="_blank"; ">Baixar</a>`; 
+            fileList.appendChild(li);
+           });
+        } else {
+            showMessage("Upload concluído, mas sem link.", "error");
+        }
+    })
 
-        
     .catch(error=> {
         console.error("Error:", error);
         showMessage("Erro ao enviar o arquivo.", "error");
